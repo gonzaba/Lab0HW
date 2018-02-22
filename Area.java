167efbf82a72a00c68abce4b35c9214eb87c7c21
Barbara P. Gonzalez <barbara.gonzalez1@upr.edu>
2018-02-21 20:47:15 -0400

    added permiter feature

--- Area.java
+++ Area.java
@@ -1,3 +1,4 @@
+
 /** 
  * @author: Barbara P. Gonzalez-Rivera 802-14-2976
  * @description: Java program to calculate the area of a circle given the radius.
@@ -15,5 +16,6 @@ public class AreaCalculation{
 	radius = input.nextInt();
 	area = Math.PI *(radius*radius);
 	System.out.print("Area is:" + area); 
-	}
+	System.out.print("Half of Area is" +radius);
+}
 }
